47e9ee3428c506a0851ebb7c4c03abd8d78af7d9
Pigpigever <Pigpigever@163.com>
2017-08-03 17:09:53 +0800

    Queue

--- Queue/queue.js
+++ Queue/queue.js
@@ -1,6 +1,43 @@
 /*
- * @Author: pigpigever 
- * @Date: 2017-08-03 16:31:54 
+ * @Author: pigpigever
+ * @Date: 2017-08-03 16:31:54
  * @Last Modified by: pigpigever
- * @Last Modified time: 2017-08-03 16:41:05
+ * @Last Modified time: 2017-08-03 17:07:25
  */
+function Queue() {
+    this.items = []
+}
+
+Queue.prototype = {
+    construtor:Queue,
+    equeue: function(item) {
+        this.items.push(item)
+    },
+    dequeue: function() {
+        return this.items.shift()
+    },
+    front: function() {
+        return this.items[0]
+    },
+    isEmpty: ()=> {
+        return this.items.length === 0
+    },
+    clear: function() {
+        this.items = []
+    },
+    size: function() {
+        return this.items.length
+    },
+    print: function() {
+        this.items.forEach(function(item) {
+                console.log(item)
+        })
+    }
+}
+
+let s = new Queue()
+
+s.equeue(1)
+s.equeue(2)
+
+s.print()
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Learning-JavaScript-Data-Structures-and-Algorithms
+# Learning-JavaScript-Data-Structures-And-Algorithms
