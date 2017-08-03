37205817fffcfb70a82b99a87804033dcd61d866
Pigpigever <Pigpigever@163.com>
2017-08-03 16:39:09 +0800

    stack and queue

---
+++ Stack/stack.js
@@ -0,0 +1,38 @@
+
+function Stack() {
+    this.items = []
+}
+
+Stack.prototype = {
+    constructor: Stack,
+    push: function (item) {
+        this.items.push(item)
+    },
+    pop: function () {
+        this.items.pop()
+    },
+    peek: function () {
+        return this.items[this.items.length - 1]
+    },
+    isEmpty: function () {
+        return this.items.length === 0
+    },
+    size: function () {
+        return this.items.length
+    },
+    clear: function () {
+        this.items = []
+    },
+    print: function () {
+        this.items.forEach(item => {
+            console.log(this.item)
+        })
+    }
+};
+
+var s = new Stack()
+
+s.push(1)
+s.push(2)
+
+console.log(s.size());  //2
